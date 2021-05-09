cf2b0603c0c7d527888d00c723812a6a59238170
João  Oliveira <joaoo.rocha1@gmail.com>
2021-05-09 20:25:18 -0300

    Subindo projeto

---
+++ functions.php
@@ -0,0 +1,55 @@
+<?php
+
+// Inserir vendedores
+function insertSeller($file) {
+
+	$seller_info['id'] = $file[0];
+	$seller_info['cpf'] = $file[1];
+	$seller_info['nome'] = $file[2];
+	$seller_info['salario'] = $file[3];
+	return($seller_info);
+}
+
+// Inserir Clientes
+function insertClient($file) {
+
+	$client_info['id'] = $file[0];
+	$client_info['cnpj'] = $file[1];
+	$client_info['nome'] = $file[2];
+	$client_info['ramo_de_atividade'] = $file[3];
+
+	return$client_info;
+}
+// Inserir vendas
+function insertSales($file) {
+
+	$sales_info['id'] = $file[0];
+	$sales_info['id_vendedor'] = $file[1];
+	$sales_info['nome_vendedor'] = $file[3];
+
+	//tratando String de vendas
+	$formater = str_replace('[','',$file[2]);
+	$formater = str_replace(']','',$formater);
+	$formater = str_replace(' ','',$formater);
+	$formater = explode(',', $formater);
+
+	$count = 1;
+
+	//Loop para inserir vendas depois de tratada
+	foreach($formater as $f) {
+		$formater2 = explode('-', strval($f));
+
+		$sales_info['venda']['lista'][] = array(
+			"id" => $sales_info['id_vendedor']." - ".$count,
+			"id_item" => $formater2[0],
+			"quantidade" => $formater2[1],
+			"preco" => $formater2[2],
+			"valor_venda" => ($formater2[1] * $formater2[2])
+		);
+		
+		$count++ ;
+
+	}
+	
+	return $sales_info;
+}
---
+++ index.php
@@ -0,0 +1,72 @@
+<!-- Atualizando o sistema a cada 1s -->
+<script>
+	setTimeout(function() {
+  		window.location.reload(1);
+	}, 1000);			
+</script>
+
+<?php
+	include('functions.php');
+
+	echo "Sistema Operando...";
+
+	// Criar pasta de entrada
+	if(!is_dir("data/in")) {
+		mkdir(__DIR__.'/data/in/', 0777, true);
+	}
+
+	// Criar pasta de saida
+	if(!is_dir("data/out")) {
+		mkdir(__DIR__.'/data/out/', 0777, true);
+	}
+
+
+	// Definindo diretorio 
+	$dh = dir ("data/in/");
+
+	// Loop para verificar arquivos
+	while ($input = $dh->read()) {
+
+		//Filtrar pelo formato 
+		$ext = pathinfo($input, PATHINFO_EXTENSION);
+
+		if($ext == "dat") {
+			$files = $input;
+			$read_file = file("data/in/".$files);
+			
+			// Loop para inserção da informação
+			foreach($read_file as $file) {
+				$explode_file = explode('ç', $file);
+
+				switch($explode_file[0]) {
+					case 001:
+						$result_seller[] = insertSeller($explode_file);
+						break;
+					case 002:
+						$result_client[] = insertClient($explode_file);
+						break;	
+					case 003:
+						$result_sales[] = insertSales($explode_file);
+						break;
+				}
+			}	
+
+		}
+
+
+	}
+
+	// Contagem dos clientes e vendedores
+	$count_seller = count($result_seller);
+	$count_client = count($result_client);
+
+
+	// Estruturando e criando arquivo de retorno
+	$arquivo = fopen('data/out/retorno.done.dat','w');
+	fwrite($arquivo, "Quantidade de clientes: ".$count_client."\r\r");
+	fwrite($arquivo, "Quantidade de Vendedor: ".$count_seller."\r\r");
+	fwrite($arquivo, "Id da venda mais cara: \r\r");
+	fwrite($arquivo, "Pior vendedor de todos os tempos: \r\r");
+
+
+?>
